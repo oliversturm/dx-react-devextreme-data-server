3349885c8c3d30d5a043a71f8ac903c25f71790f
Oliver Sturm <oliver@oliversturm.com>
2017-07-21 11:55:47 +0100

    initial checkin

---
+++ dist/data-access.js
@@ -0,0 +1,288 @@
+(function (global, factory) {
+  if (typeof define === "function" && define.amd) {
+    define(['exports', 'qs'], factory);
+  } else if (typeof exports !== "undefined") {
+    factory(exports, require('qs'));
+  } else {
+    var mod = {
+      exports: {}
+    };
+    factory(mod.exports, global.qs);
+    global.dataAccess = mod.exports;
+  }
+})(this, function (exports, _qs) {
+  'use strict';
+
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+  exports.createTestData = exports.commitChanges = exports.createDataFetcher = exports.fetchData = undefined;
+
+  var _qs2 = _interopRequireDefault(_qs);
+
+  function _interopRequireDefault(obj) {
+    return obj && obj.__esModule ? obj : {
+      default: obj
+    };
+  }
+
+  const DEFAULTBASEDATA = '//localhost:3000/data/v1/values';
+  var DEFAULTBASEAPI = '//localhost:3000/api/v1';
+
+  const createDataFetcher = (BASEDATA = DEFAULTBASEDATA) => {
+    const getSortingParams = loadOptions => loadOptions.sorting && loadOptions.sorting.length > 0 ? {
+      sort: loadOptions.sorting.map(s => ({
+        selector: s.columnName,
+        desc: s.direction === 'desc'
+      }))
+    } : {};
+
+    const getPagingParams = loadOptions => {
+      const params = {};
+      if (loadOptions.pageSize) {
+        params.take = loadOptions.pageSize;
+        params.requireTotalCount = true;
+      }
+      if (loadOptions.currentPage > 0) params.skip = loadOptions.currentPage * loadOptions.pageSize;
+      return params;
+    };
+
+    const getFilterParams = loadOptions => loadOptions.filters && loadOptions.filters.length > 0 ? {
+      filter: loadOptions.filters.reduce((r, v) => {
+        if (v.value) {
+          if (r.length > 0) r.push('and');
+          r.push([v.columnName, '=', v.columnName === 'int1' ? parseInt(v.value, 10) : v.value]);
+        }
+        return r;
+      }, [])
+    } : {};
+
+    const getGroupParams = loadOptions => loadOptions.grouping && loadOptions.grouping.length > 0 ? {
+      group: loadOptions.grouping.map(g => ({
+        selector: g.columnName,
+        isExpanded: false
+      })),
+      requireGroupCount: true,
+      skip: undefined,
+      take: undefined } : {};
+
+    const createQueryURL = (baseUrl, loadOptions) => {
+      const params = Object.assign.apply({}, [getSortingParams(loadOptions), getPagingParams(loadOptions), getFilterParams(loadOptions), getGroupParams(loadOptions)]);
+
+      console.log('Created params: ', params);
+
+      const query = _qs2.default.stringify(params, {
+        arrayFormat: 'indices'
+      });
+      return query ? baseUrl.concat('?', query) : baseUrl;
+    };
+
+    const convertSimpleQueryData = data => ({
+      rows: data.data,
+      totalCount: data.totalCount
+    });
+
+    const createExpandedGroupsString = expandedGroups => expandedGroups ? expandedGroups.join(',') : undefined;
+
+    const createGroupQueryData = (data, loadOptions) => {
+      const isExpanded = groupKey => loadOptions.expandedGroups.includes(groupKey);
+      const furtherGroupLevels = groupLevel => groupLevel + 1 < loadOptions.grouping.length;
+
+      let cqTotalCount = 0;
+      let totalCount = 0;
+
+      const pageRangeStart = loadOptions.currentPage >= 0 && loadOptions.pageSize ? loadOptions.currentPage * loadOptions.pageSize : undefined;
+      const pageRangeEnd = pageRangeStart >= 0 ? pageRangeStart + loadOptions.pageSize : undefined;
+
+      function countInPageRange(count) {
+        return pageRangeStart >= 0 ? count >= pageRangeStart && count < pageRangeEnd : true;
+      }
+
+      function groupContentOverlapsPageRange(groupStart, groupLength) {
+        return pageRangeStart >= 0 ? groupStart < pageRangeEnd && groupStart + groupLength >= pageRangeStart : true;
+      }
+
+      function* generateContentQueries(list, groupLevel = 0, parentGroupKey, parentFilters = []) {
+        function getParentFilters(group) {
+          return [...parentFilters, {
+            columnName: loadOptions.grouping[groupLevel].columnName,
+            value: group.key
+          }];
+        }
+
+        function countRow(rowsParent) {
+          if (rowsParent && isPageBoundary(cqTotalCount)) cqTotalCount++;
+
+          cqTotalCount++;
+        }
+
+        function countRows(c, rowsParent) {
+          for (let i = 0; i < c; i++) countRow(rowsParent);
+        }
+
+        for (let group of list) {
+          countRow(parentGroupKey);
+          const groupKey = (parentGroupKey ? `${parentGroupKey}|` : '') + group.key;
+          if (isExpanded(groupKey)) {
+            if (furtherGroupLevels(groupLevel)) yield* generateContentQueries(group.items, groupLevel + 1, groupKey, getParentFilters(group));else {
+              if (groupContentOverlapsPageRange(cqTotalCount, group.count)) yield {
+                groupKey,
+                queryString: createQueryURL(BASEDATA, {
+                  sorting: loadOptions.sorting,
+
+                  filters: loadOptions.filters.concat(getParentFilters(group))
+                })
+              };
+              countRows(group.count, group);
+            }
+          }
+        }
+      }
+
+      function isPageBoundary(count) {
+        const fraction = count / loadOptions.pageSize;
+        return fraction > 0 && fraction === Math.trunc(fraction);
+      }
+
+      function* generateRows(list, contentData, groupLevel = 0, parentGroupRow) {
+        function* yieldRow(row, rowsParent) {
+
+          if (rowsParent && isPageBoundary(totalCount)) {
+            const contRow = Object.assign({}, rowsParent, {
+              value: `${rowsParent.value} continued...`,
+              column: rowsParent.column
+            });
+            if (countInPageRange(totalCount)) yield contRow;
+            totalCount++;
+          }
+
+          if (countInPageRange(totalCount)) yield row;
+          totalCount++;
+        }
+
+        function createGroupRow(group) {
+          return {
+            _headerKey: `groupRow_${loadOptions.grouping[groupLevel].columnName}`,
+            key: (parentGroupRow ? `${parentGroupRow.key}|` : '') + `${group.key}`,
+            colspan: parentGroupRow ? parentGroupRow.colspan + 1 : 1,
+            value: group.key,
+            type: 'groupRow',
+            column: {
+              name: loadOptions.grouping[groupLevel].columnName,
+              title: loadOptions.grouping[groupLevel].columnName
+            },
+            rows: []
+          };
+        }
+
+        function* getGroupContent(groupRow, contentData, itemCount) {
+          const cd = contentData.find(c => c.groupKey === groupRow.key);
+          if (cd) {
+            for (let row of cd.content) yield* yieldRow(row, groupRow);
+          } else {
+            for (let i = 0; i < itemCount; i++) yield* yieldRow(null, groupRow);
+          }
+        }
+
+        for (let group of list) {
+          const groupRow = createGroupRow(group);
+          yield* yieldRow(groupRow, parentGroupRow);
+
+          if (isExpanded(groupRow.key)) {
+            if (furtherGroupLevels(groupLevel)) {
+              yield* generateRows(group.items, contentData, groupLevel + 1, groupRow);
+            } else {
+              yield* getGroupContent(groupRow, contentData, group.count);
+            }
+          }
+        }
+      }
+
+      function getContentData(groups) {
+        const queries = Array.from(generateContentQueries(groups)).map(q => simpleQuery(q.queryString).then(res => ({
+          groupKey: q.groupKey,
+          content: res.dataFetched ? res.data.rows : undefined
+        })));
+
+        return Promise.all(queries);
+      }
+
+      return getContentData(data.data).then(contentData => ({
+        rows: Array.from(generateRows(data.data, contentData)),
+        totalCount
+      }));
+    };
+
+    const simpleQuery = queryUrl => {
+      return fetch(queryUrl).then(response => response.json()).then(data => {
+        console.log('Received simple data: ', data);
+
+        return {
+          dataFetched: true,
+          data: convertSimpleQueryData(data)
+        };
+      }).catch(reason => ({
+        dataFetched: false,
+        reason
+      }));
+    };
+
+    const groupQuery = (queryUrl, loadOptions) => {
+      return fetch(queryUrl).then(response => response.json()).then(data => {
+        console.log('Received group data: ', data);
+
+        return createGroupQueryData(data, loadOptions).then(data => ({
+          dataFetched: true,
+          data
+        }));
+      }).catch(reason => ({
+        dataFetched: false,
+        reason
+      }));
+    };
+
+    return loadOptions => {
+      const queryUrl = createQueryURL(BASEDATA, loadOptions);
+
+      return new Promise((resolve, reject) => {
+        console.warn('Querying (decoded): ', decodeURIComponent(queryUrl));
+
+        (loadOptions.grouping && loadOptions.grouping.length > 0 ? groupQuery(queryUrl, loadOptions) : simpleQuery(queryUrl)).then(result => resolve(result));
+      });
+    };
+  };
+
+  const fetchData = createDataFetcher();
+
+  function sendChange(row, add = true, key) {
+    console.log(`Sending change with add=${add}, key=${key}: `, row);
+
+    const params = {
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      method: add ? 'POST' : 'PUT',
+      body: JSON.stringify(row)
+    };
+    const url = DEFAULTBASEDATA + (add ? '' : `/${key}`);
+    fetch(url, params).catch(r => console.log('Something went wrong POSTing this row: ', row));
+  }
+
+  const commitChanges = ({ added, changed, deleted }) => {
+    console.log('committing added: ', added);
+    console.log('committing changes: ', changed);
+
+    if (added && added.length > 0) for (const row of added) sendChange(row);
+    if (changed) for (const key in changed) sendChange(changed[key], false, key);
+  };
+
+  const createTestData = (BASEAPI = DEFAULTBASEAPI) => {
+    fetch(BASEAPI + '/createTestData?count=' + encodeURIComponent(1000));
+  };
+
+  exports.fetchData = fetchData;
+  exports.createDataFetcher = createDataFetcher;
+  exports.commitChanges = commitChanges;
+  exports.createTestData = createTestData;
+});
---
+++ dist/index.js
@@ -0,0 +1,168 @@
+(function (global, factory) {
+  if (typeof define === "function" && define.amd) {
+    define(['exports', 'react', 'prop-types', '@devexpress/dx-react-core', './data-access', './loading.css'], factory);
+  } else if (typeof exports !== "undefined") {
+    factory(exports, require('react'), require('prop-types'), require('@devexpress/dx-react-core'), require('./data-access'), require('./loading.css'));
+  } else {
+    var mod = {
+      exports: {}
+    };
+    factory(mod.exports, global.react, global.propTypes, global.dxReactCore, global.dataAccess, global.loading);
+    global.index = mod.exports;
+  }
+})(this, function (exports, _react, _propTypes, _dxReactCore, _dataAccess) {
+  'use strict';
+
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+
+  var _react2 = _interopRequireDefault(_react);
+
+  var _propTypes2 = _interopRequireDefault(_propTypes);
+
+  function _interopRequireDefault(obj) {
+    return obj && obj.__esModule ? obj : {
+      default: obj
+    };
+  }
+
+  const defaultLoadingIndicator = () => _react2.default.createElement(
+    'div',
+    { className: 'loading-shading' },
+    _react2.default.createElement('span', { className: 'glyphicon glyphicon-refresh loading-icon' })
+  );
+
+  class DevExtremeDataServer extends _react2.default.PureComponent {
+    constructor(props) {
+      super(props);
+      this.state = {
+        loadResult: undefined,
+        reloadState: undefined,
+        loading: false
+      };
+      this.getRows = this.getRows.bind(this);
+      this.getTotalCount = this.getTotalCount.bind(this);
+
+      this.fetchData = (0, _dataAccess.createDataFetcher)(this.props.url);
+    }
+
+    getRows() {
+      return this.state.loadResult ? this.state.loadResult.rows : [];
+    }
+
+    getTotalCount() {
+      return this.state.loadResult ? this.state.loadResult.totalCount : 0;
+    }
+
+    getData(loadOptions) {
+      const loadingTimer = setTimeout(() => {
+        this.setState({
+          showLoadingIndicator: true
+        });
+      }, this.props.loadingIndicatorThreshold);
+
+      this.fetchData(loadOptions).then(res => {
+        if (res.dataFetched) {
+          clearTimeout(loadingTimer);
+
+          this.setState({
+            reloadState: this.props.reloadState,
+            loading: false,
+            showLoadingIndicator: false,
+            loadResult: {
+              rows: res.data.rows,
+              totalCount: res.data.totalCount
+            }
+          });
+        }
+      });
+    }
+
+    getLoadOptions() {
+      return {
+        sorting: this.state.sorting,
+        currentPage: this.state.currentPage,
+        pageSize: this.state.pageSize,
+        filters: this.state.filters,
+        grouping: this.state.grouping,
+        expandedGroups: this.state.expandedGroups
+      };
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+      if (this.props.reloadState !== prevProps.reloadState && !this.state.loading) this.setState({
+        loading: true
+      });
+      if (prevState.sorting !== this.state.sorting || prevState.currentPage !== this.state.currentPage || prevState.pageSize !== this.state.pageSize || prevState.filters !== this.state.filters || prevState.grouping !== this.state.grouping || prevState.expandedGroups !== this.state.expandedGroups || prevProps.reloadState !== this.props.reloadState) this.getData(this.getLoadOptions());
+    }
+
+    render() {
+      return _react2.default.createElement(
+        _dxReactCore.PluginContainer,
+        null,
+        _react2.default.createElement(_dxReactCore.Watcher, {
+          watch: getter => ['sorting', 'currentPage', 'pageSize', 'filters', 'grouping', 'expandedGroups'].map(getter),
+          onChange: (action, ...vals) => {
+            const newPage = this.state.pageSize >= 0 && this.state.pageSize !== vals[2] ? Math.trunc(vals[1] * this.state.pageSize / vals[2]) : vals[1];
+
+            this.setState({
+              sorting: vals[0],
+              currentPage: newPage,
+              pageSize: vals[2],
+              filters: vals[3],
+              grouping: vals[4],
+              expandedGroups: vals[5] ? Array.from(vals[5].values()) : [],
+              loading: true
+            });
+            if (newPage !== vals[1]) action('setCurrentPage')({
+              page: newPage
+            });
+          }
+        }),
+        _react2.default.createElement(_dxReactCore.Getter, { name: 'totalCount', value: this.getTotalCount() }),
+        _react2.default.createElement(_dxReactCore.Getter, { name: 'rows', value: this.getRows() }),
+        _react2.default.createElement(_dxReactCore.Getter, { name: 'loading', value: this.state.loading }),
+        _react2.default.createElement(_dxReactCore.Getter, {
+          name: 'totalPages',
+          pureComputed: (totalCount, pageSize) => pageSize ? Math.ceil(totalCount / pageSize) : 0,
+          connectArgs: getter => [getter('totalCount'), getter('pageSize')]
+        }),
+        _react2.default.createElement(_dxReactCore.Watcher, {
+          watch: getter => [getter('totalPages'), getter('currentPage')],
+          onChange: (action, totalPages, currentPage) => {
+            if (totalPages > 0 && totalPages - 1 <= currentPage) action('setCurrentPage')({ page: Math.max(totalPages - 1, 0) });
+          }
+        }),
+        _react2.default.createElement(
+          _dxReactCore.Template,
+          { name: 'root' },
+          _react2.default.createElement(
+            'div',
+            null,
+            _react2.default.createElement(_dxReactCore.TemplatePlaceholder, null),
+            this.state.showLoadingIndicator && this.props.useLoadingIndicator && this.props.loadingIndicator()
+          )
+        )
+      );
+    }
+  }
+
+  DevExtremeDataServer.defaultProps = {
+    url: undefined,
+    reloadState: 0,
+    loadingIndicator: defaultLoadingIndicator,
+    useLoadingIndicator: true,
+    loadingIndicatorThreshold: 500
+  };
+
+  DevExtremeDataServer.propTypes = {
+    url: _propTypes2.default.string,
+    reloadState: _propTypes2.default.number,
+    loadingIndicator: _propTypes2.default.func,
+    useLoadingIndicator: _propTypes2.default.bool,
+    loadingIndicatorThreshold: _propTypes2.default.number
+  };
+
+  exports.default = DevExtremeDataServer;
+});
